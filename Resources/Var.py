595dff484e2ad5c01b9eec11ce9908482c9e90cf
ThePythonFury <71390859+ThePythonFury@users.noreply.github.com>
2020-10-22 13:27:36 -0400

    Var.py

---
+++ Resources/Var.py
@@ -0,0 +1,69 @@
+#These are all of the conversion values!
+
+
+"""Made by @ThePythonFury on Github, and by my helper, Mr. Baumgardner"""
+
+
+milestokilo = 1.609
+milestometer = 1609.34
+milestoyard = 1760
+milestofoot = 5280
+milestomm = 1.609e+6
+milestoinch = 63360
+milestocm = 160934
+
+kilotomile = 0.621371
+kilotometer = 1000
+kilotoyard = 1093.61
+kilotofoot = 3280.84
+kilotomm = 1000000
+kilotoinch = 39370.1
+kilotocm = 100000
+
+metertomile = 0.000621371
+metertokilo = 0.001
+metertomm = 1000
+metertocm = 100
+metertoinch = 39.3701
+metertoyard = 1.093613888889
+metertofoot = 3.28084
+
+yardtomile = 0.000568182
+yardtokilo = 0.000914
+yardtometer = 0.9144
+yardtofoot = 3.0000009
+yardtomm = 914.400
+yardtoinch = 36.00001
+yardtocm = 91.44003
+
+feettomile = 6.213713910761155838e-6
+feettokilo = 0.0003048
+feettometer = 0.3048
+feettoyard = 0.333333
+feettomm = 304.7996952
+feettoinch = 11.999988
+feettocm = 30.48
+
+mmtomile = 6.2137e-7
+mmtokilo = 1e-6
+mmtometer = 0.001
+mmtofoot = 0.00328084
+mmtoinch = 0.0393701
+mmtocm = 0.1
+mmtoyard = 0.00109361
+
+inchtomile = 1.5783e-5
+inchtokilo = 2.54e-5
+inchtometer = 0.0254
+inchtoyard = 0.0277778
+inchtomm = 25.4
+inchtocm = 2.54
+inchtofoot = 0.0833333
+
+cmtomile = 6.2137e-6
+cmtokilo = 1e-5
+cmtometer = 0.01
+cmtoyard = 0.0109361
+cmtomm = 10
+cmtofeet = 0.0328084
+cmtoinch = 0.393701
