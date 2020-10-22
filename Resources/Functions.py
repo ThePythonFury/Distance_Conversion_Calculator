4543820a4837f0c8d733ece5700b6eb434a2b05c
ThePythonFury <71390859+ThePythonFury@users.noreply.github.com>
2020-10-22 13:26:08 -0400

    Functions.py

---
+++ Resources/Functions.py
@@ -0,0 +1,27 @@
+#These are all of the function values!
+
+
+"""Made by @ThePythonFury on Github, and by my helper, Mr. Baumgardner"""
+
+import os
+from random import randint as rt
+from Resources.Var import *
+import time as tm
+from Resources.Colors import *
+def Clear():
+  os.system('clear')
+
+def RWT():
+  RT = rt(1,5)
+  if RT == 1:
+    tm.sleep(2)
+  if RT == 2:
+    (1.2)
+  if RT == 3:
+    tm.sleep(0.8)
+  if RT == 4:
+    tm.sleep(2.4)
+  if RT == 5:
+    tm.sleep(1)
+
+
